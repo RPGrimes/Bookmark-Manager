be5b05c9887c5b57c49058140da1ce1f37ab8fa8
Ryan Grimes <ryanpaulgrimes@gmail.com>
2021-11-10 14:53:21 +0100

    Update database name

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -3,7 +3,7 @@ require 'pg'
 class Bookmarks
 
   def self.all
-    connection = PG.connect :dbname => 'postgres'
+    connection = PG.connect :dbname => 'bookmark_manager'
     result = connection.exec "SELECT * FROM bookmarks"
     result.map do |bookmark|
       bookmark['url']
