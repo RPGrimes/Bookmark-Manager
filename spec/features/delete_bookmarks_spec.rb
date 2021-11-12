011b0feba12b338c4c3ee01471020198afcba97c
Adam Harbisher <aharbisher@gmail.com>
2021-11-12 11:50:31 +0000

    erb working in browser but failing rspec

--- spec/features/delete_bookmarks_spec.rb
+++ spec/features/delete_bookmarks_spec.rb
@@ -4,9 +4,15 @@ feature 'delete bookmarks' do
   scenario 'A user can delete a chosen bookmark' do
     connection = PG.connect(dbname: 'bookmark_manager_test')
     Bookmarks.add('https://store.steampowered.com/', 'steam')
+    Bookmarks.add('https://google.com/', 'Google')
     visit('/bookmarks')
     expect(page).to have_link("steam", :href=>"https://store.steampowered.com/")
-    find("[id='delete-1']").click
+    expect(page).to have_link("Google", :href=>"https://google.com/")
+    page.should have_css('div#delete-1')
+    within('div#delete-1') do
+      click_button "Delete"
+    end
     expect(page).to_not have_link("steam", :href=>"https://store.steampowered.com/")
+    expect(page).to have_link("Google", :href=>"https://google.com/")
   end
 end
